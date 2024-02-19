1fdd696e5d30a76398a950409bd94d0a12e5fc73
FathimaNooriya <fathimanooriyakn@gmail.com>
2024-02-19 21:31:29 +0530

    map points added

--- lib/locationmap.dart
+++ lib/locationmap.dart
@@ -9,12 +9,23 @@ class LocationMap extends StatelessWidget {
 
   final LatLng collegeLat = LatLng(10.902752012224196, 76.12170400178304);
   final LatLng startingLat = LatLng(10.871674657929175, 75.93404357896162);
+  final LatLng valancheryLat = LatLng(10.887939, 76.073187);
+  final LatLng thriunavayaLat = LatLng(10.865801, 75.984058);
+  final LatLng kanchipuraLat = LatLng(10.916988, 76.040974);
+  final LatLng vettichiraLat = LatLng(10.928812, 76.023188);
+  final LatLng puthanathaniLat = LatLng(10.934784, 76.005294);
 
   @override
   Widget build(BuildContext context) {
     final Polyline _kPolyline = Polyline(
       polylineId: PolylineId('route'),
-      points: [collegeLat, startingLat],
+      points: [
+        collegeLat,
+        valancheryLat,
+        puthanathaniLat,
+        thriunavayaLat,
+        startingLat,
+      ],
       color: Colors.blue,
       width: 5,
     );
@@ -26,6 +37,31 @@ class LocationMap extends StatelessWidget {
       ),
       polylines: {_kPolyline},
       markers: {
+        Marker(
+          markerId: MarkerId("puthanathani"),
+          position: puthanathaniLat,
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+        ),
+        Marker(
+          markerId: MarkerId("vettichira"),
+          position: vettichiraLat,
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+        ),
+        Marker(
+          markerId: MarkerId("kanchipura"),
+          position: kanchipuraLat,
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+        ),
+        Marker(
+          markerId: MarkerId("thirunavaya"),
+          position: thriunavayaLat,
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+        ),
+        Marker(
+          markerId: MarkerId("valanachery"),
+          position: valancheryLat,
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+        ),
         Marker(
           markerId: MarkerId("starting"),
           position: startingLat,

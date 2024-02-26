0fb0eee6f1b468b940490cc7887615a78d5ede80
FathimaNooriya <fathimanooriyakn@gmail.com>
2024-02-26 13:45:15 +0530

    admin dashboard changed

--- lib/admin.dart
+++ lib/admin.dart
@@ -1,13 +1,14 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
 import 'package:collage_bus_nufa/parent.dart';
-import 'package:collage_bus_nufa/restart.dart';
 import 'package:collage_bus_nufa/show_feedback.dart';
 import 'package:collage_bus_nufa/student.dart';
 
 class Admin extends StatelessWidget {
-  const Admin({Key? key}) : super(key: key);
+  Admin({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -30,57 +31,105 @@ class Admin extends StatelessWidget {
           mainAxisSpacing: 80, // Vertical space between cards
           childAspectRatio: aspectRatio, // Adjust based on your content
           children: <Widget>[
-            itemDashboard('Student Details', CupertinoIcons.person_2,
-                Colors.deepOrange, parent()),
-            itemDashboard('Teacher Details', CupertinoIcons.person_3,
-                Colors.green, student()),
-            itemDashboard(
-                'Restart', CupertinoIcons.restart, Colors.blue, restart()),
-            itemDashboard('Feedback', CupertinoIcons.mail, Colors.purple,
-                show_feedback()),
+            GestureDetector(
+                child: itemDashboard(
+                    background: Colors.deepOrange,
+                    iconData: CupertinoIcons.person_2,
+                    title: 'Student Details'),
+                onTap: () {
+                  Get.to(parent());
+                }),
+            GestureDetector(
+                child: itemDashboard(
+                    background: Colors.green,
+                    iconData: CupertinoIcons.person_3,
+                    title: 'Teacher Details'),
+                onTap: () {
+                  Get.to(student());
+                }),
+            GestureDetector(
+                child: itemDashboard(
+                    background: Colors.blue,
+                    iconData: CupertinoIcons.restart,
+                    title: 'Restart'),
+                onTap: () {
+                  restartApp();
+                }),
+            GestureDetector(
+                child: itemDashboard(
+                    background: Colors.purple,
+                    iconData: CupertinoIcons.mail,
+                    title: 'Feedback'),
+                onTap: () {
+                  Get.to(show_feedback());
+                }),
           ],
         ),
       ),
     );
   }
+}
 
-  Widget itemDashboard(
-      String title, IconData iconData, Color background, Widget push) {
-    return GestureDetector(
-      onTap: () {
-        Get.to(() => push);
-      },
-      child: Card(
-        elevation: 3,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15.0),
+restartApp() {
+  Get.defaultDialog(
+      title: 'Notification', // To display the title it is optional
+      content: Text('Restarted'), // Message which will be pop up on the screen
+      // Action widget which will provide the user to acknowledge the choice
+      actions: [
+        TextButton(
+          // FlatButton widget is used to make a text to work like a button
+          onPressed: () {
+            Get.back();
+          }, // function used to perform after pressing the button
+          child: Text('Ok'),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              padding: const EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                color: background,
-                shape: BoxShape.circle,
-              ),
-              child: Icon(
-                iconData,
-                color: Colors.white,
-                size: 50,
-              ),
+      ]);
+}
+
+class itemDashboard extends StatelessWidget {
+  itemDashboard({
+    Key? key,
+    required this.title,
+    required this.background,
+    required this.iconData,
+  }) : super(key: key);
+
+  final String title;
+  final Color background;
+  final IconData iconData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 3,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(15),
+            decoration: BoxDecoration(
+              color: background,
+              shape: BoxShape.circle,
             ),
-            const SizedBox(height: 10),
-            Text(
-              title,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.bold,
-              ),
+            child: Icon(
+              iconData,
+              color: Colors.white,
+              size: 50,
             ),
-          ],
-        ),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            title,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
       ),
     );
   }
